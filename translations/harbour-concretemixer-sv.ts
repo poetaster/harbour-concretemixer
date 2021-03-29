d454da712b87867a84b130f36022bc15afb64032
Åke Engelbrektson <eson57@users.noreply.github.com>
2021-03-29 12:19:16 +0200

    Create harbour-concretemixer-sv.ts
    Add Swedish translation please.
    Thanks for taking over maintenance.

---
+++ translations/harbour-concretemixer-sv.ts
@@ -0,0 +1,185 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Concrete</source>
+        <translation>Betong</translation>
+    </message>
+    <message>
+        <source>Mixer</source>
+        <translation>Blandare</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>calculate</source>
+        <translation>Beräkna</translation>
+    </message>
+    <message>
+        <source>slab / plate</source>
+        <translation>Platta</translation>
+    </message>
+    <message>
+        <source>cylinder</source>
+        <translation>Cylinder</translation>
+    </message>
+    <message>
+        <source>length [m]</source>
+        <translation>längd [m]</translation>
+    </message>
+    <message>
+        <source>width [m]</source>
+        <translation>bredd [m]</translation>
+    </message>
+    <message>
+        <source>height [m]</source>
+        <translation>höjd [m]</translation>
+    </message>
+    <message>
+        <source>radius [m]</source>
+        <translation>radie [m]</translation>
+    </message>
+    <message>
+        <source>MANUAL</source>
+        <translation>MANUELLT</translation>
+    </message>
+    <message>
+        <source>cement</source>
+        <translation>cement</translation>
+    </message>
+    <message>
+        <source>NECESSARY MATERIAL</source>
+        <translation>NÖDVÄNDIGT MATERIAL</translation>
+    </message>
+    <message>
+        <source>kg</source>
+        <translation>kg</translation>
+    </message>
+    <message>
+        <source>m³</source>
+        <translation>m³</translation>
+    </message>
+    <message>
+        <source>50kg bags</source>
+        <translation>50 kg-säck</translation>
+    </message>
+    <message>
+        <source>amount of water to add to the mix [liter]</source>
+        <translation>Mängd vatten att tillsätta blandningen [liter]</translation>
+    </message>
+    <message>
+        <source>VOLUME RESULTS</source>
+        <translation>VOLYMRESULTAT</translation>
+    </message>
+    <message>
+        <source>of which </source>
+        <translation>av vilket </translation>
+    </message>
+    <message>
+        <source>and </source>
+        <translation>och </translation>
+    </message>
+    <message>
+        <source>% is entrained air [m³]</source>
+        <translation>% är inblandad luft [m³]</translation>
+    </message>
+    <message>
+        <source>OTHER RESULTS</source>
+        <translation>ANDRA RESULTAT</translation>
+    </message>
+    <message>
+        <source>estimated weight as ready-mix [kg]</source>
+        <translation>Uppskattad vikt som färdig blandning [kg]</translation>
+    </message>
+    <message>
+        <source>calculated total weight [kg]</source>
+        <translation>Beräknad totalvikt [kg]</translation>
+    </message>
+    <message>
+        <source>area load [kN/m²]</source>
+        <translation>Arealast [kN/m²]</translation>
+    </message>
+    <message>
+        <source>CONCRETE MIXER</source>
+        <translation>BETONGBLANDARE</translation>
+    </message>
+    <message>
+        <source>mix-ratio</source>
+        <translation>Blandningsförhållande</translation>
+    </message>
+    <message>
+        <source>aggregates</source>
+        <translation>aggregeringar</translation>
+    </message>
+    <message>
+        <source>gravel / coarse</source>
+        <translation>grus / grov</translation>
+    </message>
+    <message>
+        <source>&gt; 4,75 mm</source>
+        <translation>&gt; 4,75 mm</translation>
+    </message>
+    <message>
+        <source>sand / fine</source>
+        <translation>sand / fin</translation>
+    </message>
+    <message>
+        <source>&lt; 4,75 mm</source>
+        <translation>&lt; 4,75 mm</translation>
+    </message>
+    <message>
+        <source>C10/M10 - weak, 10N/mm² after 28 days</source>
+        <translation>C10/M10 - Svag, 10N/mm² efter 28 dagar</translation>
+    </message>
+    <message>
+        <source>C15/M15 - general purpose, 15N/mm² after 28 days</source>
+        <translation>C15/M15 - Allmänt syfte, 15N/mm² efter 28 dagar</translation>
+    </message>
+    <message>
+        <source>C25/M25 - strong, 25N/mm² after 28 days</source>
+        <translation>C25/M25 - Stark, 25N/mm² efter 28 dagar</translation>
+    </message>
+    <message>
+        <source>C30/M30 - 30N/mm² after 28 days</source>
+        <translation>C30/M30 - 30N/mm² efter 28 dagar</translation>
+    </message>
+    <message>
+        <source>C40/M40 - 40N/mm² after 28 days</source>
+        <translation>C40/M40 - 40N/mm² efter 28 dagar</translation>
+    </message>
+    <message>
+        <source>C20/M20 - normal, 20N/mm² after 28 days</source>
+        <translation>C20/M20 - Normal, 20N/mm² efter 28 dagar</translation>
+    </message>
+    <message>
+        <source>CONCRETE TO ORDER</source>
+        <translation>BETONG ATT BESTÄLLA</translation>
+    </message>
+    <message>
+        <source>% is material [m³]</source>
+        <translation>% är material [m³]</translation>
+    </message>
+    <message>
+        <source>object volume [m³]</source>
+        <translation>Objektvolym [m³]</translation>
+    </message>
+    <message>
+        <source>object</source>
+        <translation>Objekt:</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Item</source>
+        <translation>Objekt</translation>
+    </message>
+    <message>
+        <source>Fine Tuning</source>
+        <translation>Fininställning</translation>
+    </message>
+</context>
+</TS>
