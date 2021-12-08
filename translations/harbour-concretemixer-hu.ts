f0d66d46c1da9333299ab1584884865f0fb016c8
1Zgp <36508793+1Zgp@users.noreply.github.com>
2021-12-08 14:51:24 +0200

    Create harbour-concretemixer-hu.ts

---
+++ translations/harbour-concretemixer-hu.ts
@@ -0,0 +1,185 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Concrete</source>
+        <translation>Beton</translation>
+    </message>
+    <message>
+        <source>Mixer</source>
+        <translation>Keverő</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>calculate</source>
+        <translation>számolás</translation>
+    </message>
+    <message>
+        <source>slab / plate</source>
+        <translation>tábla / lemez</translation>
+    </message>
+    <message>
+        <source>cylinder</source>
+        <translation>henger</translation>
+    </message>
+    <message>
+        <source>length [m]</source>
+        <translation>hosszúság [m]</translation>
+    </message>
+    <message>
+        <source>width [m]</source>
+        <translation>szélesség [m]</translation>
+    </message>
+    <message>
+        <source>height [m]</source>
+        <translation>magasság [m]</translation>
+    </message>
+    <message>
+        <source>radius [m]</source>
+        <translation>sugár [m]</translation>
+    </message>
+    <message>
+        <source>MANUAL</source>
+        <translation>KÉZI</translation>
+    </message>
+    <message>
+        <source>cement</source>
+        <translation>cement</translation>
+    </message>
+    <message>
+        <source>NECESSARY MATERIAL</source>
+        <translation>SZÜKSÉGES ANYAG</translation>
+    </message>
+    <message>
+        <source>kg</source>
+        <translation>kg</translation>
+    </message>
+    <message>
+        <source>m³</source>
+        <translation>m³</translation>
+    </message>
+    <message>
+        <source>50kg bags</source>
+        <translation>50 kilós zsákok</translation>
+    </message>
+    <message>
+        <source>amount of water to add to the mix [liter]</source>
+        <translation>a keverékhez adandó víz mennyisége [liter]</translation>
+    </message>
+    <message>
+        <source>VOLUME RESULTS</source>
+        <translation>MENNYISÉGI EREDMÉNYEK</translation>
+    </message>
+    <message>
+        <source>of which </source>
+        <translation>amiből</translation>
+    </message>
+    <message>
+        <source>and </source>
+        <translation>és </translation>
+    </message>
+    <message>
+        <source>% is entrained air [m³]</source>
+        <translation>% belekevert levegő [m³]</translation>
+    </message>
+    <message>
+        <source>OTHER RESULTS</source>
+        <translation>EGYÉB EREDMÉNYEK</translation>
+    </message>
+    <message>
+        <source>estimated weight as ready-mix [kg]</source>
+        <translation>kész keverék becsült tömege [kg]</translation>
+    </message>
+    <message>
+        <source>calculated total weight [kg]</source>
+        <translation>számított teljes tömeg [kg]</translation>
+    </message>
+    <message>
+        <source>area load [kN/m²]</source>
+        <translation>terület terhelése [kN/m²]</translation>
+    </message>
+    <message>
+        <source>CONCRETE MIXER</source>
+        <translation>BETONKEVERŐ</translation>
+    </message>
+    <message>
+        <source>mix-ratio</source>
+        <translation>keverési arány</translation>
+    </message>
+    <message>
+        <source>aggregates</source>
+        <translation>adalékanyagok</translation>
+    </message>
+    <message>
+        <source>gravel / coarse</source>
+        <translation>sóder / durva</translation>
+    </message>
+    <message>
+        <source>&gt; 4,75 mm</source>
+        <translation>&gt; 4,75 mm</translation>
+    </message>
+    <message>
+        <source>sand / fine</source>
+        <translation>homok / finom</translation>
+    </message>
+    <message>
+        <source>&lt; 4,75 mm</source>
+        <translation>&lt; 4,75 mm</translation>
+    </message>
+    <message>
+        <source>C10/M10 - weak, 10N/mm² after 28 days</source>
+        <translation>C10/M10 - gyenge, 10N/mm² 28 nap után</translation>
+    </message>
+    <message>
+        <source>C15/M15 - general purpose, 15N/mm² after 28 days</source>
+        <translation>C15/M15 - általános felhasználású, 15N/mm² 28 nap után</translation>
+    </message>
+    <message>
+        <source>C25/M25 - strong, 25N/mm² after 28 days</source>
+        <translation>C25/M25 - erős, 25N/mm² 28 nap után</translation>
+    </message>
+    <message>
+        <source>C30/M30 - 30N/mm² after 28 days</source>
+        <translation>C30/M30 - 30N/mm² 28 nap után</translation>
+    </message>
+    <message>
+        <source>C40/M40 - 40N/mm² after 28 days</source>
+        <translation>C40/M40 - 40N/mm² 28 nap után</translation>
+    </message>
+    <message>
+        <source>C20/M20 - normal, 20N/mm² after 28 days</source>
+        <translation>C20/M20 - normális, 20N/mm² 28 nap után</translation>
+    </message>
+    <message>
+        <source>CONCRETE TO ORDER</source>
+        <translation>RENDELENDŐ BETON</translation>
+    </message>
+    <message>
+        <source>% is material [m³]</source>
+        <translation>% anyag [m³]</translation>
+    </message>
+    <message>
+        <source>object volume [m³]</source>
+        <translation>objektum térfogat [m³]</translation>
+    </message>
+    <message>
+        <source>object</source>
+        <translation>objektum</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Item</source>
+        <translation>Elem</translation>
+    </message>
+    <message>
+        <source>Fine Tuning</source>
+        <translation>Finomhangolás</translation>
+    </message>
+</context>
+</TS>
