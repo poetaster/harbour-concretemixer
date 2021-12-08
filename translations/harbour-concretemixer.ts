eb97e2ee49f204b9fa9ce100cc2a719d3fd0f157
1Zgp <36508793+1Zgp@users.noreply.github.com>
2021-12-08 14:34:55 +0200

    Update harbour-concretemixer.ts

--- translations/harbour-concretemixer.ts
+++ translations/harbour-concretemixer.ts
@@ -5,181 +5,181 @@
     <name>CoverPage</name>
     <message>
         <source>Concrete</source>
-        <translation type="unfinished"></translation>
+        <translation>Beton</translation>
     </message>
     <message>
         <source>Mixer</source>
-        <translation type="unfinished"></translation>
+        <translation>Keverő</translation>
     </message>
 </context>
 <context>
     <name>FirstPage</name>
     <message>
         <source>calculate</source>
-        <translation type="unfinished"></translation>
+        <translation>számolás</translation>
     </message>
     <message>
         <source>slab / plate</source>
-        <translation type="unfinished"></translation>
+        <translation>tábla / lemez</translation>
     </message>
     <message>
         <source>cylinder</source>
-        <translation type="unfinished"></translation>
+        <translation>henger</translation>
     </message>
     <message>
         <source>length [m]</source>
-        <translation type="unfinished"></translation>
+        <translation>hosszúság [m]</translation>
     </message>
     <message>
         <source>width [m]</source>
-        <translation type="unfinished"></translation>
+        <translation>szélesség [m]</translation>
     </message>
     <message>
         <source>height [m]</source>
-        <translation type="unfinished"></translation>
+        <translation>magasság [m]</translation>
     </message>
     <message>
         <source>radius [m]</source>
-        <translation type="unfinished"></translation>
+        <translation>sugár [m]</translation>
     </message>
     <message>
         <source>MANUAL</source>
-        <translation type="unfinished"></translation>
+        <translation>KÉZI</translation>
     </message>
     <message>
         <source>cement</source>
-        <translation type="unfinished"></translation>
+        <translation>cement</translation>
     </message>
     <message>
         <source>NECESSARY MATERIAL</source>
-        <translation type="unfinished"></translation>
+        <translation>SZÜKSÉGES ANYAG</translation>
     </message>
     <message>
         <source>kg</source>
-        <translation type="unfinished"></translation>
+        <translation>kg</translation>
     </message>
     <message>
         <source>m³</source>
-        <translation type="unfinished"></translation>
+        <translation>m³</translation>
     </message>
     <message>
         <source>50kg bags</source>
-        <translation type="unfinished"></translation>
+        <translation>50 kilós zsákok</translation>
     </message>
     <message>
         <source>amount of water to add to the mix [liter]</source>
-        <translation type="unfinished"></translation>
+        <translation>a keverékhez adandó víz mennyisége [liter]</translation>
     </message>
     <message>
         <source>VOLUME RESULTS</source>
-        <translation type="unfinished"></translation>
+        <translation>MENNYISÉGI EREDMÉNYEK</translation>
     </message>
     <message>
         <source>of which </source>
-        <translation type="unfinished"></translation>
+        <translation>amiből</translation>
     </message>
     <message>
         <source>and </source>
-        <translation type="unfinished"></translation>
+        <translation>és </translation>
     </message>
     <message>
         <source>% is entrained air [m³]</source>
-        <translation type="unfinished"></translation>
+        <translation>% belekevert levegő [m³]</translation>
     </message>
     <message>
         <source>OTHER RESULTS</source>
-        <translation type="unfinished"></translation>
+        <translation>EGYÉB EREDMÉNYEK</translation>
     </message>
     <message>
         <source>estimated weight as ready-mix [kg]</source>
-        <translation type="unfinished"></translation>
+        <translation>kész keverék becsült tömege [kg]</translation>
     </message>
     <message>
         <source>calculated total weight [kg]</source>
-        <translation type="unfinished"></translation>
+        <translation>számított teljes tömeg [kg]</translation>
     </message>
     <message>
         <source>area load [kN/m²]</source>
-        <translation type="unfinished"></translation>
+        <translation>terület terhelése [kN/m²]</translation>
     </message>
     <message>
         <source>CONCRETE MIXER</source>
-        <translation type="unfinished"></translation>
+        <translation>BETONKEVERŐ</translation>
     </message>
     <message>
         <source>mix-ratio</source>
-        <translation type="unfinished"></translation>
+        <translation>keverési arány</translation>
     </message>
     <message>
         <source>aggregates</source>
-        <translation type="unfinished"></translation>
+        <translation>adalékanyagok</translation>
     </message>
     <message>
         <source>gravel / coarse</source>
-        <translation type="unfinished"></translation>
+        <translation>sóder / durva</translation>
     </message>
     <message>
         <source>&gt; 4,75 mm</source>
-        <translation type="unfinished"></translation>
+        <translation>&gt; 4,75 mm</translation>
     </message>
     <message>
         <source>sand / fine</source>
-        <translation type="unfinished"></translation>
+        <translation>homok / finom</translation>
     </message>
     <message>
         <source>&lt; 4,75 mm</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt; 4,75 mm</translation>
     </message>
     <message>
         <source>C10/M10 - weak, 10N/mm² after 28 days</source>
-        <translation type="unfinished"></translation>
+        <translation>C10/M10 - gyenge, 10N/mm² 28 nap után</translation>
     </message>
     <message>
         <source>C15/M15 - general purpose, 15N/mm² after 28 days</source>
-        <translation type="unfinished"></translation>
+        <translation>C15/M15 - általános felhasználású, 15N/mm² 28 nap után</translation>
     </message>
     <message>
         <source>C25/M25 - strong, 25N/mm² after 28 days</source>
-        <translation type="unfinished"></translation>
+        <translation>C25/M25 - erős, 25N/mm² 28 nap után</translation>
     </message>
     <message>
         <source>C30/M30 - 30N/mm² after 28 days</source>
-        <translation type="unfinished"></translation>
+        <translation>C30/M30 - 30N/mm² 28 nap után</translation>
     </message>
     <message>
         <source>C40/M40 - 40N/mm² after 28 days</source>
-        <translation type="unfinished"></translation>
+        <translation>C40/M40 - 40N/mm² 28 nap után</translation>
     </message>
     <message>
         <source>C20/M20 - normal, 20N/mm² after 28 days</source>
-        <translation type="unfinished"></translation>
+        <translation>C20/M20 - normális, 20N/mm² 28 nap után</translation>
     </message>
     <message>
         <source>CONCRETE TO ORDER</source>
-        <translation type="unfinished"></translation>
+        <translation>RENDELENDŐ BETON</translation>
     </message>
     <message>
         <source>% is material [m³]</source>
-        <translation type="unfinished"></translation>
+        <translation>% anyag [m³]</translation>
     </message>
     <message>
         <source>object volume [m³]</source>
-        <translation type="unfinished"></translation>
+        <translation>objektum térfogat [m³]</translation>
     </message>
     <message>
         <source>object</source>
-        <translation type="unfinished"></translation>
+        <translation>objektum</translation>
     </message>
 </context>
 <context>
     <name>Settings</name>
     <message>
         <source>Item</source>
-        <translation type="unfinished"></translation>
+        <translation>Elem</translation>
     </message>
     <message>
         <source>Fine Tuning</source>
-        <translation type="unfinished"></translation>
+        <translation>Finomhangolás</translation>
     </message>
 </context>
 </TS>
