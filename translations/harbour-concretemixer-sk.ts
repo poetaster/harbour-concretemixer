159a109dee6bdbb4ecca436b46cd6c5e1710b7f8
okruhliak <ladislav@hodas.sk>
2023-06-25 16:25:35 +0200

    Update harbour-concretemixer-sk.ts

---
+++ translations/harbour-concretemixer-sk.ts
@@ -0,0 +1,185 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk_SK">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Concrete</source>
+        <translation>Betón</translation>
+    </message>
+    <message>
+        <source>Mixer</source>
+        <translation>Miešačka</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>calculate</source>
+        <translation>vypočítať</translation>
+    </message>
+    <message>
+        <source>slab / plate</source>
+        <translation>kváder/doska</translation>
+    </message>
+    <message>
+        <source>cylinder</source>
+        <translation>valec</translation>
+    </message>
+    <message>
+        <source>length [m]</source>
+        <translation>dĺžka [m]</translation>
+    </message>
+    <message>
+        <source>width [m]</source>
+        <translation>šírka [m]</translation>
+    </message>
+    <message>
+        <source>height [m]</source>
+        <translation>výška [m]</translation>
+    </message>
+    <message>
+        <source>radius [m]</source>
+        <translation>polomer [m]</translation>
+    </message>
+    <message>
+        <source>MANUAL</source>
+        <translation>NÁVOD</translation>
+    </message>
+    <message>
+        <source>cement</source>
+        <translation>cement</translation>
+    </message>
+    <message>
+        <source>NECESSARY MATERIAL</source>
+        <translation>POTREBNÝ MATERIÁL</translation>
+    </message>
+    <message>
+        <source>kg</source>
+        <translation>kg</translation>
+    </message>
+    <message>
+        <source>m³</source>
+        <translation>m³</translation>
+    </message>
+    <message>
+        <source>50kg bags</source>
+        <translation>50 kg vrecia</translation>
+    </message>
+    <message>
+        <source>amount of water to add to the mix [liter]</source>
+        <translation>množstvo vody pridané do miešačky [l]</translation>
+    </message>
+    <message>
+        <source>VOLUME RESULTS</source>
+        <translation>OBJEMOVÉ MNOŽSTVÁ</translation>
+    </message>
+    <message>
+        <source>of which </source>
+        <translation>z toho </translation>
+    </message>
+    <message>
+        <source>and </source>
+        <translation>a </translation>
+    </message>
+    <message>
+        <source>% is entrained air [m³]</source>
+        <translation>% tvorí vzduch [m³]</translation>
+    </message>
+    <message>
+        <source>OTHER RESULTS</source>
+        <translation>OSTATNÉ MNOŽSTVÁ</translation>
+    </message>
+    <message>
+        <source>estimated weight as ready-mix [kg]</source>
+        <translation>odhadovaná hmotnosť hotovej zmesi [kg]</translation>
+    </message>
+    <message>
+        <source>calculated total weight [kg]</source>
+        <translation>vypočítaná celková hmotnosť [kg]</translation>
+    </message>
+    <message>
+        <source>area load [kN/m²]</source>
+        <translation>plošné zaťaženie [kN/m²]</translation>
+    </message>
+    <message>
+        <source>CONCRETE MIXER</source>
+        <translation>MIEŠAČKA BETÓNU</translation>
+    </message>
+    <message>
+        <source>mix-ratio</source>
+        <translation>pomery miešania</translation>
+    </message>
+    <message>
+        <source>aggregates</source>
+        <translation>zložky</translation>
+    </message>
+    <message>
+        <source>gravel / coarse</source>
+        <translation>štrk / hrubé</translation>
+    </message>
+    <message>
+        <source>&gt; 4,75 mm</source>
+        <translation>&gt; 4,75 mm</translation>
+    </message>
+    <message>
+        <source>sand / fine</source>
+        <translation>piesok / jemné</translation>
+    </message>
+    <message>
+        <source>&lt; 4,75 mm</source>
+        <translation>&lt; 4,75 mm</translation>
+    </message>
+    <message>
+        <source>C10/M10 - weak, 10N/mm² after 28 days</source>
+        <translation>C10/M10 - slabý, 10N/mm² po 28 dňoch</translation>
+    </message>
+    <message>
+        <source>C15/M15 - general purpose, 15N/mm² after 28 days</source>
+        <translation>C15/M15 - bežné použitie, 15N/mm² po 28 dňoch</translation>
+    </message>
+    <message>
+        <source>C25/M25 - strong, 25N/mm² after 28 days</source>
+        <translation>C25/M25 - pevný, 25N/mm² po 28 dňoch</translation>
+    </message>
+    <message>
+        <source>C30/M30 - 30N/mm² after 28 days</source>
+        <translation>C30/M30 - 30N/mm² po 28 dňoch</translation>
+    </message>
+    <message>
+        <source>C40/M40 - 40N/mm² after 28 days</source>
+        <translation>C40/M40 - 40N/mm² po 28 dňoch</translation>
+    </message>
+    <message>
+        <source>C20/M20 - normal, 20N/mm² after 28 days</source>
+        <translation>C20/M20 - bežný, 20N/mm² po 28 dňoch</translation>
+    </message>
+    <message>
+        <source>CONCRETE TO ORDER</source>
+        <translation>BETÓN NA OBJEDNANIE</translation>
+    </message>
+    <message>
+        <source>% is material [m³]</source>
+        <translation>% je materiál [m³]</translation>
+    </message>
+    <message>
+        <source>object volume [m³]</source>
+        <translation>množstvo pre objekt [m³]</translation>
+    </message>
+    <message>
+        <source>object</source>
+        <translation>objekt</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Item</source>
+        <translation>Položka</translation>
+    </message>
+    <message>
+        <source>Fine Tuning</source>
+        <translation>Jemné nastavenie</translation>
+    </message>
+</context>
+</TS>
